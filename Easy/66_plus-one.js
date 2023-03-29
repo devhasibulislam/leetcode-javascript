65219e955a59ca73344c2cdc0f455e35866709dd
hasibulislam999 <hasib143sl@gmail.com>
2023-03-29 17:44:44 +0600

    Plus One problem solved

---
+++ Easy/66_plus-one.js
@@ -0,0 +1,24 @@
+/**
+ * Title: Plus One
+ * Description: You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+ * Author: Hasibul Islam
+ * Date: 29/03/2023
+ */
+
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function (digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i]++;
+      return digits;
+    }
+    digits[i] = 0;
+  }
+  digits.unshift(1);
+  return digits;
+};
+
+console.log(plusOne([1, 2, 3]));
