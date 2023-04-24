f2c297a10b1855f6375c4cdf89d2982c7def3436
hasibulislam999 <hasib143sl@gmail.com>
2023-04-24 15:54:53 +0600

    Maximum Product of the Length of Two Palindromic Substrings problem solved

--- Rolling Hash/1960_maximum-product-of-the-length-of-two-palindromic-substrings.js
+++ Rolling Hash/1960_maximum-product-of-the-length-of-two-palindromic-substrings.js
@@ -1,63 +1,59 @@
 /**
  * Title: Maximum Product of the Length of Two Palindromic Substrings
- * Description: You are given a 0-indexed string s and are tasked with finding two non-intersecting palindromic substrings of odd length such that the product of their lengths is maximized.
+ * Description: You are given a 0-indexed string s and are tasked with finding two non-intersecting palindromic substrings of odd length such that the product of their lengths is maximized. More formally, you want to choose four integers i, j, k, l such that 0 <= i <= j < k <= l < s.length and both the substrings s[i...j] and s[k...l] are palindromes and have odd lengths. s[i...j] denotes a substring from index i to index j inclusive. Return the maximum possible product of the lengths of the two non-intersecting palindromic substrings. A palindrome is a string that is the same forward and backward. A substring is a contiguous sequence of characters in a string.
  * Author: Hasibul Islam
  * Date: 24/04/2023
  */
-
-function isPalindrome(s) {
-  let i = 0,
-    j = s.length - 1;
-  while (i < j) {
-    if (s[i] !== s[j]) {
-      return false;
-    }
-    i++;
-    j--;
-  }
-  return true;
-}
 
 /**
  * @param {string} s
  * @return {number}
  */
 function maxProduct(s) {
-  const n = s.length;
-  const palindromes = [];
+  let n = s.length;
+
+  let dp = new Array(n).fill(0);
+  let before = new Array(n).fill(0);
+  let after = new Array(n).fill(0);
+
+  let c = -1;
+  let r = -1;
+
   for (let i = 0; i < n; i++) {
-    for (let j = 0; i - j >= 0 && i + j < n; j++) {
-      if (
-        s[i - j] === s[i + j] &&
-        (j === 0 || isPalindrome(s.substring(i - j + 1, i + j)))
-      ) {
-        palindromes.push([i - j, i + j]);
-      } else {
-        break;
-      }
+    let k = i <= r ? Math.min(dp[2 * c - i], r - i) : 0;
+    let p = i - k;
+    let q = i + k;
+
+    while (p >= 0 && q < n && s[p] === s[q]) {
+      before[q] = Math.max(before[q], q - p + 1);
+      after[p] = Math.max(after[p], q - p + 1);
+      p--;
+      q++;
     }
-    for (let j = 0; i - j >= 0 && i + j + 1 < n; j++) {
-      if (
-        s[i - j] === s[i + j + 1] &&
-        (j === 0 || isPalindrome(s.substring(i - j + 1, i + j + 2)))
-      ) {
-        palindromes.push([i - j, i + j + 1]);
-      } else {
-        break;
-      }
+
+    dp[i] = q - i - 1;
+
+    if (q - 1 > r) {
+      c = i;
+      r = q - 1;
     }
   }
-  let maxProduct = 0;
-  for (let i = 0; i < palindromes.length; i++) {
-    for (let j = i + 1; j < palindromes.length; j++) {
-      const [i1, j1] = palindromes[i];
-      const [i2, j2] = palindromes[j];
-      if (i2 > j1 || j2 < i1) {
-        maxProduct = Math.max(maxProduct, (j1 - i1 + 1) * (j2 - i2 + 1));
-      }
-    }
+
+  for (let i = 1; i < n; i++) {
+    before[i] = Math.max(before[i - 1], before[i]);
   }
-  return maxProduct;
+
+  for (let i = n - 2; i >= 0; i--) {
+    after[i] = Math.max(after[i + 1], after[i]);
+  }
+
+  let result = 0;
+
+  for (let i = 1; i < n; i++) {
+    result = Math.max(result, before[i - 1] * after[i]);
+  }
+
+  return result;
 }
 
-console.log(maxProduct("ababbb")); // Output: 9 (palindromic substrings are "aba" and "bbb")
+console.log(maxProduct("zaaaxbbby"));
