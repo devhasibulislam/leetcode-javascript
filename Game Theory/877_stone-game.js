1f0cc3316fe3603ceadea7607fa60245e534a99b
hasibulislam999 <hasib143sl@gmail.com>
2023-05-09 19:19:06 +0600

    Stone Game problem solved

---
+++ Game Theory/877_stone-game.js
@@ -0,0 +1,14 @@
+/**
+ * Title: Stone Game
+ * Description: Alice and Bob play a game with piles of stones. There are an even number of piles arranged in a row, and each pile has a positive integer number of stones piles[i].
+ * Author: Hasibul Islam
+ * Date: 06/05/2023
+ */
+
+/**
+ * @param {number[]} piles
+ * @return {boolean}
+ */
+var stoneGame = function (piles) {
+  return true;
+};
