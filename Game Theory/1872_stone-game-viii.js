e0510ec9e366fd0f7f99cf786bbfaa28d8c31a30
hasibulislam999 <hasib143sl@gmail.com>
2023-05-09 19:32:27 +0600

    Stone Game VII problem solved

---
+++ Game Theory/1872_stone-game-viii.js
@@ -0,0 +1,30 @@
+/**
+ * Title: Stone Game VII
+ * Description: Alice and Bob take turns playing a game, with Alice starting first.
+ * Author: Hasibul Islam
+ * Date: 06/05/2023
+ */
+
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var stoneGameVIII = function (stones) {
+  let prefix = Array(stones.length);
+  for (let i = 0; i < stones.length; i++)
+    prefix[i] = stones[i] + (prefix[i - 1] || 0);
+
+  let dp = Array(stones.length).fill(0);
+  function game(idx) {
+    if (dp[idx]) return dp[idx];
+    if (idx >= stones.length) return 0;
+    let skip = game(idx + 1);
+    let take = prefix[idx] - skip;
+    dp[idx] = take;
+    // Both Alice and Bob must maximize their scores, which would maximize the difference for Alice and minimize the difference for Bob
+    // Return the max score of either removing stones from the current idx or skipping to the next idx
+    return !skip ? take : Math.max(skip, take);
+  }
+
+  return game(1);
+};
